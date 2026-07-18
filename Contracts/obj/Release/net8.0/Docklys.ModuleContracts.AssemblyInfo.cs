3218240f1d93e794040b6ef5c2a1efb84cc093b3
qwqc <lz@live.de>
2026-07-18 18:05:30 +0200

    Update module files

--- Contracts/obj/Release/net8.0/Docklys.ModuleContracts.AssemblyInfo.cs
+++ Contracts/obj/Release/net8.0/Docklys.ModuleContracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Docklys.ModuleContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+8779929589e91a48d9224b5286e514d0ed7d79b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+9c1c4540d574b3d24b86eb9dbca354c67d2beea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Docklys.ModuleContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Docklys.ModuleContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
--- obj/Debug/net9.0/scrcpy.AssemblyInfo.cs
+++ obj/Debug/net9.0/scrcpy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scrcpy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+8779929589e91a48d9224b5286e514d0ed7d79b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+9c1c4540d574b3d24b86eb9dbca354c67d2beea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("scrcpy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scrcpy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
