8779929589e91a48d9224b5286e514d0ed7d79b2
qwqc <lz@live.de>
2026-07-18 13:46:32 +0200

    Update module files

--- obj/Release/net9.0/scrcpy.AssemblyInfo.cs
+++ obj/Release/net9.0/scrcpy.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("scrcpy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04d9c47ab0e43cffc755dacf6e39b365f75875c")]
 [assembly: System.Reflection.AssemblyProductAttribute("scrcpy")]
